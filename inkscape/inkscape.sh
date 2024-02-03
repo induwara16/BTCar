ffd6a0080c8ed126c9349f263a06e644fe3836d5
Induwara <induwara16@yandex.com>
2024-02-03 20:19:32 +0530

    Added alien command to generate rpm file

--- inkscape/inkscape.sh
+++ inkscape/inkscape.sh
@@ -16,8 +16,6 @@ inkscape ./logo.svg -o ./tmp/favicon-64.png -w 64
 inkscape ./logo.svg -o ./tmp/favicon-128.png -w 128
 inkscape ./logo.svg -o ./tmp/favicon-256.png -w 256
 
-inkscape ./logo.svg -o ../images/BTCar.png -w 256
-
 cd ./tmp
 
 convert ./favicon-32.png ./favicon-64.png ./favicon-128.png ./favicon-256.png ./favicon.ico
